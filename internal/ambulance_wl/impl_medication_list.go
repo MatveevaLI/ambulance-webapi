433aaf4d63b5a9e4e70f910495c78d39dd97565f
MatveevaLI <ladaivanna.matveeva@gmail.com>
2024-05-11 11:58:41 +0200

    Kostra servera

---
+++ internal/ambulance_wl/impl_medication_list.go
@@ -0,0 +1,34 @@
+package ambulance_wl
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Nasledujúci kód je kópiou vygenerovaného a zakomentovaného kódu zo súboru api_ambulance_waiting_list.go
+
+// CreateMedicationListEntry - Saves new entry into waiting list
+func (this *implMedicationListAPI) CreateMedicationListEntry(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+// DeleteMedicationListEntry - Deletes specific entry
+func (this *implMedicationListAPI) DeleteMedicationListEntry(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+// GetMedicationListEntries - Provides the ambulance waiting list
+func (this *implMedicationListAPI) GetMedicationListEntries(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+// GetMedicationListEntry - Provides details about waiting list entry
+func (this *implMedicationListAPI) GetMedicationListEntry(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+// UpdateMedicationListEntry - Updates specific entry
+func (this *implMedicationListAPI) UpdateMedicationListEntry(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
