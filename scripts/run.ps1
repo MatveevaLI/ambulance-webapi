439a14e0f48145f08149a2a338071f40e161826d
MatveevaLI <ladaivanna.matveeva@gmail.com>
2024-05-11 14:32:16 +0200

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -32,6 +32,9 @@ switch ($command) {
     "mongo" {
     mongo up
     }
+    "docker" {
+       docker build -t matveevali/ambulance-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
     default {
         throw "Unknown command: $command"
     }
